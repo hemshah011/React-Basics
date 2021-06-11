bcf7c8070c6a7db1235cbfc868e1ccdb7cc9d21b
hemshah011 <hemshah011@gmail.com>
2021-06-11 14:36:08 +0530

    Refs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import ParentComp from './components/ParentComp'
+//import ParentComp from './components/ParentComp'
+import RefsDemo from './components/RefsDemo'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ParentComp/>
+        <RefsDemo/>
+        {/* <ParentComp/> */}
       </header>
     </div>
   );
